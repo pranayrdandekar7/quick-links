0c9091d39091ec0c66384b90534bac875939f157
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-08-07 11:37:27 +0530

    basic react app is completed

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import{createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from "./views/Home/Home.js"
+
+const router = createBrowserRouter([
+{
+  path:'/',
+  element:<Home/>
+}
+
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router ={router}/>);
+
+
+
